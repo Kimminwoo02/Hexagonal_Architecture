665a9397f23b67181dfeb55eae70337426749720
minutaurus <made_power@naver.com>
2023-11-01 23:05:13 +0900

    Hexagonal 아키텍처 실습

---
+++ src/test/java/com/example/minupay/MinuPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minupay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinuPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
