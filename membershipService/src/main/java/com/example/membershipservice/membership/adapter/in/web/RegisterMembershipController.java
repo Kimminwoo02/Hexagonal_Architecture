e0bbb84e1dc15571df6609bf801bcdb90c899005
minutaurus <made_power@naver.com>
2023-11-02 22:30:06 +0900

    Controller 및 DTO

---
+++ membershipService/src/main/java/com/example/membershipservice/membership/adapter/in/web/RegisterMembershipController.java
@@ -0,0 +1,43 @@
+package com.example.membershipservice.membership.adapter.in.web;
+
+import com.example.membershipservice.membership.application.port.in.RegisterMembershipCommand;
+import com.example.membershipservice.membership.application.port.in.RegisterMembershipUseCase;
+import com.example.membershipservice.membership.common.WebAdapter;
+import com.example.membershipservice.membership.domain.Membership;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+// webAdapter
+@WebAdapter
+@RestController
+@RequiredArgsConstructor
+public class RegisterMembershipController {
+
+    private final RegisterMembershipUseCase registerMembershipUseCase;
+
+    @PostMapping("/membership/register/")
+    Membership registerMembership(@RequestBody RegisterMembershipRequest request){
+        // request~~~
+
+        // request -> Command
+
+        //useCase ~~ (request)
+
+        RegisterMembershipCommand command = RegisterMembershipCommand.builder()
+                .name(request.getName())
+                .address(request.getAddress())
+                .email(request.getEmail())
+                .isValid(true)
+                .isCorp(request.getIsCorp())
+                .build();
+
+
+        registerMembershipUseCase.registerMembership(command);
+    }
+
+
+
+}
