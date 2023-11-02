e971a5bd3f7c912de7ded62b06fa936cb5c6fe63
minutaurus <made_power@naver.com>
2023-11-02 22:29:41 +0900

    Adapter 추가

---
+++ membershipService/src/main/java/com/example/membershipservice/membership/adapter/out/persistence/MembershipPersistenceAdapter.java
@@ -0,0 +1,25 @@
+package com.example.membershipservice.membership.adapter.out.persistence;
+
+import com.example.membershipservice.membership.application.port.out.RegisterMembershipPort;
+import com.example.membershipservice.membership.common.PersistenceAdapter;
+import com.example.membershipservice.membership.domain.Membership;
+import lombok.RequiredArgsConstructor;
+
+@PersistenceAdapter
+@RequiredArgsConstructor
+public class MembershipPersistenceAdapter implements RegisterMembershipPort {
+
+    private final SpringDataMembershipRepository membershipRepository;
+    @Override
+    public void createMembership(Membership.MembershipName membershipName, Membership.MembershipEmail membershipEmail, Membership.MembershipAddress membershipAddress, Membership.MembershipIsValid membershipIsValid, Membership.MembershipIsCorp membershipIsCorp) {
+        membershipRepository.save(
+                new MembershipJpaEntity(  
+                        membershipName.getNameValue(),
+                        membershipEmail.getEmailValue(),
+                        membershipAddress.getAddressValue(),
+                        membershipIsValid.isValidValue(),
+                        membershipIsCorp.isCorpValue()
+                )
+        );
+    }
+}
