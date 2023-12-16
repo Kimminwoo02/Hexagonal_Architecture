1db37bb6a2c6c162b76111cafa230157a31f0646
minutaurus <made_power@naver.com>
2023-12-16 14:23:18 +0900

    MemberShipAdapter 에 Modify 포트 구현

--- membershipService/src/main/java/com/example/membershipservice/membership/adapter/out/persistence/MembershipPersistenceAdapter.java
+++ membershipService/src/main/java/com/example/membershipservice/membership/adapter/out/persistence/MembershipPersistenceAdapter.java
@@ -1,5 +1,6 @@
 package com.example.membershipservice.membership.adapter.out.persistence;
 
+import com.example.membershipservice.membership.application.port.out.ModifyMembershipPort;
 import com.example.membershipservice.membership.application.port.out.RegisterMembershipPort;
 import com.example.membershipservice.common.PersistenceAdapter;
 import com.example.membershipservice.membership.domain.Membership;
@@ -7,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 
 @PersistenceAdapter
 @RequiredArgsConstructor
-public class MembershipPersistenceAdapter implements RegisterMembershipPort {
+public class MembershipPersistenceAdapter implements RegisterMembershipPort, ModifyMembershipPort {
 
     private final SpringDataMembershipRepository membershipRepository;
     @Override
@@ -21,5 +22,18 @@ public class MembershipPersistenceAdapter implements RegisterMembershipPort {
                         membershipIsCorp.isCorpValue()
                 )
         );
+    }
+
+    @Override
+    public MembershipJpaEntity modifyMembership(Membership.MembershipId membershipId, Membership.MembershipName membershipName, Membership.MembershipEmail membershipEmail, Membership.MembershipAddress membershipAddress, Membership.MembershipIsCorp membershipIsCorp, Membership.MembershipIsValid membershipIsValid) {
+        MembershipJpaEntity entity = membershipRepository.getReferenceById(Long.parseLong(membershipId.getMembershipId()));
+
+        entity.setName(membershipName.getNameValue());
+        entity.setAddress(membershipAddress.getAddressValue());
+        entity.setEmail(membershipEmail.getEmailValue());
+        entity.setCorp(membershipIsCorp.isCorpValue());
+        entity.setValid(membershipIsValid.isValidValue());
+        return membershipRepository.save(entity);
+
     }
 }
